b040197f4c3796d169ac196b9e301e3d53d154b9
Eylam Tagor <eylamtagor@gmail.com>
2025-11-29 13:28:23 -0500

    causal gail for antmaze

--- causal_rl/algo/imitation/gail/core_net.py
+++ causal_rl/algo/imitation/gail/core_net.py
@@ -112,42 +112,6 @@ class ContinuousActor(nn.Module):
         entropy = base.entropy()
         return log_prob, entropy
 
-    def forward(self, z):
-        out1 = self.activation_fn(self.linear1(z))
-        out2 = self.activation_fn(self.linear2(out1))
-        out3 = self.activation_fn(self.linear3(out1 + out2))
-
-        mu = self.linear4(out1 + out2 + out3)
-        std = self.log_std_actor.exp().expand_as(mu)
-        dist = Normal(mu, std)
-        return Independent(dist, 1)
-
-    @torch.no_grad()
-    def act(self, z, deterministic=False):
-        base = self.forward(z)
-        u = base.base_dist.loc if deterministic else base.rsample()
-
-        a_tanh = torch.tanh(u)
-        action = (a_tanh + 1) * 0.5 * (self.high - self.low) + self.low
-
-        log_det_tanh = torch.log(1 - a_tanh.pow(2) + 1e-6).sum(dim=-1)
-        log_det_scale = u.shape[-1] * torch.log(torch.as_tensor((self.high - self.low)/2.0, device=z.device, dtype=z.dtype)).sum()
-
-        return action, (base.log_prob(u) - (log_det_tanh + log_det_scale)), base.entropy()
-
-    def evaluate_actions(self, z, action):
-        base = self.forward(z)
-
-        a_tanh = ((action - self.low) / (self.high - self.low) * 2) - 1.0
-        a_tanh = torch.clamp(a_tanh, -0.999, 0.999)
-
-        u = 0.5 * (torch.log1p(a_tanh + 1e-6) - torch.log1p(-a_tanh + 1e-6))
-
-        log_det_tanh = torch.log(1 - a_tanh.pow(2) + 1e-6).sum(dim=-1)
-        log_det_scale = u.shape[-1] * torch.log(torch.as_tensor((self.high - self.low)/2.0, device=z.device, dtype=z.dtype)).sum()
-
-        return (base.log_prob(u) - (log_det_tanh + log_det_scale)), base.entropy()
-
 class DiscreteActor(nn.Module):
     def __init__(self, num_inputs, num_outputs, hidden_size=1024, activation='relu'):
         super(DiscreteActor, self).__init__()
